a03781c3eaf1b1e64cbb50b204238c4998a068c4
w1nterm00n <kordesovad@gmail.com>
2024-07-28 00:09:07 +0200

    added is and displaying messages by ID

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const messages = [
 	{
+		id: 1,
 		text: "Hello world",
 		user: "Someone",
 		added: new Date(),
@@ -20,8 +21,25 @@ router.use((req, res, next) => {
 router.post("/new", (req, res) => {
 	const messageText = req.body.messageInput;
 	const messageUser = req.body.authorInput;
-	messages.push({ text: messageText, user: messageUser, added: new Date() });
+	const messageId = messages.length + 1;
+	messages.push({
+		id: messageId,
+		text: messageText,
+		user: messageUser,
+		added: new Date(),
+	});
 	res.redirect("/"); // Перенаправляем на главную страницу после добавления сообщения
+});
+
+// Пример маршрута для отображения сообщения по ID
+router.get("/open/:id", (req, res) => {
+	const messageId = parseInt(req.params.id, 10);
+	const message = messages.find((m) => m.id === messageId);
+	if (message) {
+		res.render("messageInfoPage", { message: message });
+	} else {
+		res.status(404).send("Message not found");
+	}
 });
 
 router.get("/", indexController.renderIndex);
